ead82dcc1df2afcb9873a08814e04f76b89dd367
Alexandre Paiva <alexandredba510@gmail.com>
2024-05-12 17:09:43 -0300

    Atualização com inclusão de estrutura de decisão (if, if else ) e repetição (do while)

--- src/funcoes.js
+++ src/funcoes.js
@@ -1,31 +1,29 @@
 
-
-
 // Função para escolher o nivel
 function nivelXP(quantidadeXp){
-    let nivel;
+let nivel;
 // Estruturas de decisões 
-if (quantidadeXp  > 0 && quantidadeXp < 1000) { 
+if (quantidadeXp  > 0 && quantidadeXp <= 1000) { 
     nivel ='Ferro'; 
 } 
-else if (quantidadeXp  > 1001 && quantidadeXp < 2000) { 
+else if (quantidadeXp  > 1001 && quantidadeXp <= 2000) { 
     nivel ='Bronze';
 }
-else if (quantidadeXp  > 2001 && quantidadeXp < 5000) { 
+else if (quantidadeXp  > 2001 && quantidadeXp <= 5000) { 
     nivel ='Prata';
 }
-else if (quantidadeXp  > 5001 && quantidadeXp < 7000) { 
+else if (quantidadeXp  > 5001 && quantidadeXp <= 7000) { 
     nivel ='Ouro';
 }
-else if (quantidadeXp  > 7001 && quantidadeXp < 8000) { 
+else if (quantidadeXp  > 7001 && quantidadeXp <= 8000) { 
     nivel ='Platina';
 }
-else if (quantidadeXp  > 8001 && quantidadeXp < 9000) { 
+else if (quantidadeXp  > 8001 && quantidadeXp <= 9000) { 
     nivel ='Ascendente';
 }
-else if (quantidadeXp  > 9001 && quantidadeXp < 10000) { 
+else if (quantidadeXp  > 9001 && quantidadeXp <= 10000) { 
     nivel ='Imortal';}
-else if (quantidadeXp  > 10001) { 
+else if (quantidadeXp  >= 10001) { 
     nivel ='Radiante'; 
 }
 else {
@@ -34,9 +32,11 @@ else {
  return nivel
 };
 
+
 // Função para imprimir 
-function print(nomeHeroi, nivel) {
-    console.log(`O Herói de nome **${nomeHeroi}** está no nível de **${nivel}**`);
+function print(nomeHeroi, nivel, quantidadeXp) {
+        console.log(`O Herói de nome **${nomeHeroi}** está no nível de **${nivel}**com ${quantidadeXp} XP`);
     };
 
-export {nivelXP, print};
+
+export {print, nivelXP};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 //Importando funçõas:
-import{nivelXP, print} from './funcoes.js';
+import{print, nivelXP} from './funcoes.js';
 //importar readline-sync para interação
 import readline from 'readline-sync';
+let i = 1;
+let quantidadeXp;
+const nomeHeroi = readline.question('Digite o nome do heroi: '); // Entrada de dados.
+console.log("========================================================================\n");
+        console.log(`Seja Bem-vindo ${nomeHeroi}`);
+const quantidade = readline.question('Digite a quantidade XP do heroi: '); // Entrada de dados.
+do {
+     i++; 
+    if(i === 2 ) {
+    // Chamado para função nível através do XP do herói
+        console.log(`${nomeHeroi} você venceu o vilão e recebeu +250 XP`);
+        } else if (i === 3 ) {
+        // Chamado para função nível através do XP do herói
+        console.log(`${nomeHeroi} você venceu o vilão e recebeu +250 XP`);
+        } else if (i === 4 ) {
+        // Chamado para função nível através do XP do herói
+        console.log(`${nomeHeroi} você venceu o vilão e recebeu +250 XP`);
+        };
+    quantidadeXp = parseInt(quantidade) + (i * 250); 
+    let nivel = nivelXP(quantidadeXp);
+    // Chamando a função print (Imprimir)
+    print(nomeHeroi, nivel, quantidadeXp);
+    console.log("========================================================================\n");
+    } while(i <= 4) {
+    
+    console.log("================================= THE END ================================\n");
 
-
-const nomeHeroi = readline.question('Digite o nome do herói: '); // Entrada de dados.
-const quantidadeXp = readline.question('Digite a quantidade XP do herói: '); // Entrada de dados.
-console.log(`Seja Bem-vindo ${nomeHeroi} seu XP atual é de ${quantidadeXp}`);
-
-// Chamado para função nível através do XP do herói
-let nivel = nivelXP(quantidadeXp);
-
-// Chamando a função print (Imprimir)
-print(nomeHeroi, nivel);
+    }
