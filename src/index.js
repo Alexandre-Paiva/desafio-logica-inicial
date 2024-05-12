0dadb2b09603f9e2af74dde1d57a5e53af89d0f6
Alexandre Paiva <alexandredba510@gmail.com>
2024-05-12 18:46:27 -0300

    Atualização de correção

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ const nomeHeroi = readline.question('Digite o nome do heroi: '); // Entrada de d
 console.log("========================================================================\n");
         console.log(`Seja Bem-vindo ${nomeHeroi}`);
 const quantidade = readline.question('Digite a quantidade XP do heroi: '); // Entrada de dados.
+console.log("========================================================================\n");
 do {
      i++; 
     if(i === 2 ) {
     // Chamado para função nível através do XP do herói
-        console.log(`${nomeHeroi} você venceu o vilão e recebeu +250 XP`);
+        console.log(`${nomeHeroi} você venceu uma legiao de mostros e recebeu +500 XP`);
         } else if (i === 3 ) {
         // Chamado para função nível através do XP do herói
         console.log(`${nomeHeroi} você venceu o vilão e recebeu +250 XP`);
@@ -20,13 +21,14 @@ do {
         // Chamado para função nível através do XP do herói
         console.log(`${nomeHeroi} você venceu o vilão e recebeu +250 XP`);
         };
-    quantidadeXp = parseInt(quantidade) + (i * 250); 
-    let nivel = nivelXP(quantidadeXp);
-    // Chamando a função print (Imprimir)
-    print(nomeHeroi, nivel, quantidadeXp);
+        quantidadeXp = parseInt(quantidade) + (i * 250); 
+        let nivel = nivelXP(quantidadeXp);
+        // Chamando a função print (Imprimir)
+        print(nomeHeroi, nivel, quantidadeXp);
     console.log("========================================================================\n");
     } while(i <= 4) {
     
+    console.log(`Parabéns ${nomeHeroi}, você é o nosso herói !!  `)   
     console.log("================================= THE END ================================\n");
 
     }
